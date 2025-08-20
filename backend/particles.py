fff1253f7a1bbdcfba1e76fc787cdfa35a8671a7
R-0-H-A-N <80087401+R-0-H-A-N@users.noreply.github.com>
2025-08-20 12:05:23 +0530

    Added slight changes to particles file

--- backend/particles.py
+++ backend/particles.py
@@ -19,7 +19,7 @@ def view_articles(username):
     rows = cursor.fetchall()
     conn.close()
 
-    return [{'article_id': row[0], 'title': row[1], 'content': row[2]} for row in rows]
+    return [{'particle_id': row[0], 'title': row[1], 'content': row[2]} for row in rows]
 
 def search_article(username, search_term):
     """
@@ -43,7 +43,7 @@ def search_article(username, search_term):
     return [{'article_id': row[0], 'title': row[1], 'content': row[2]} for row in rows]
 
 
-def delete_article(article_id):
+def delete_article(particle_id):
     """
     Delete article by article_id. Returns True if deleted, False otherwise.
     
@@ -53,10 +53,25 @@ def delete_article(article_id):
     """
     conn = sqlite3.connect('db/pim.db')
     cursor = conn.cursor()
-    cursor.execute("DELETE FROM particles WHERE article_id = ?", (article_id,))
+    cursor.execute("DELETE FROM particles WHERE article_id = ?", (particle_id,))
     conn.commit()
     deleted = cursor.rowcount > 0
 
     conn.close()
 
     return deleted
+
+def edit_particle(username, password, particle_id):
+    """
+    This funciton allows for the updating of particles. Such as saving new changes to the title or content
+    # TODO: Write this function.
+    """
+    pass
+
+def particle_views_count(particle_id):
+    """
+    This functions is somewhat of a counter for the number of times a particle has been viewed.
+    # TODO: Maybe make a feature where the most viewed particles are showed higher up on the list when searching for particles. Or make more viewed particles more favoured for instant searches regardless of similarity.
+    # TODO: Maybe add a weight system to the particles that weighs between similarity and popularity
+    """
+    pass
